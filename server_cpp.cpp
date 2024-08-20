8b459dcf3ebcd2e00df19ce27ab36ec17b6cf671
Muhammet Mutlu <mumutlu@k2m10s05.42kocaeli.com.tr>
2024-08-20 04:01:41 +0300

    socket cpp

--- server_cpp.cpp
+++ server_cpp.cpp
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
+#include <vector>
 
 #define PORT 8080
 
@@ -10,15 +11,18 @@ int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    char buffer[1024] = {0};
+    char buffer[1024];
+    std::vector<int> clients;
 
     // Server socket oluşturma
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     // Server adresini yapılandırma
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
@@ -37,57 +41,67 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Select için fd_set yapılarını oluşturma
-    fd_set read_fds;
+    fd_set read_fds, write_fds;
     int max_fd = server_socket;
 
     while (true) {
-        // Her döngüde fd_set'i sıfırlama
         FD_ZERO(&read_fds);
+        FD_ZERO(&write_fds);
         FD_SET(server_socket, &read_fds);
+        max_fd = server_socket;
 
-        // Bağlı bir client varsa, onu da set'e ekleme
-        if (client_socket > 0) {
-            FD_SET(client_socket, &read_fds);
-            max_fd = (client_socket > server_socket) ? client_socket : server_socket;
+        for (size_t i = 0; i < clients.size(); ++i) {
+            FD_SET(clients[i], &read_fds);
+            FD_SET(clients[i], &write_fds);
+            if (clients[i] > max_fd) max_fd = clients[i];
         }
 
-        // select çağrısını yapma
-        int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+        int activity = select(max_fd + 1, &read_fds, &write_fds, NULL, NULL);
         if (activity < 0) {
             perror("select error");
             close(server_socket);
             exit(EXIT_FAILURE);
         }
 
-        // Yeni bir bağlantı var mı?
         if (FD_ISSET(server_socket, &read_fds)) {
-            if ((client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len)) < 0) {
+            client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+            if (client_socket < 0) {
                 perror("accept");
                 close(server_socket);
                 exit(EXIT_FAILURE);
             }
             std::cout << "New connection established" << std::endl;
+            clients.push_back(client_socket);
         }
 
-        // Client'tan veri okuma
-        if (FD_ISSET(client_socket, &read_fds)) {
-            int valread = read(client_socket, buffer, 1024);
-            if (valread > 0) {
-                std::cout << "Received: " << buffer << std::endl;
-                // Buffer'daki veriyi geri gönderme (echo)
-                send(client_socket, buffer, valread, 0);
-                std::cout << "Sent: " << buffer << std::endl;
-            } else if (valread == 0) {
-                // Bağlantı kapandı
-                std::cout << "Client disconnected" << std::endl;
-                close(client_socket);
-                client_socket = 0;  // client_socket'ı sıfırla
-            } else {
-                perror("read error");
-                close(client_socket);
-                client_socket = 0;
+        for (std::vector<int>::iterator it = clients.begin(); it != clients.end(); ) {
+            int sd = *it;
+            if (FD_ISSET(sd, &read_fds)) {
+                int valread = read(sd, buffer, 1024);
+                if (valread > 0) {
+                    buffer[valread] = '\0';
+                    std::cout << "Received from client: " << buffer << std::endl;
+
+                    for (size_t i = 0; i < clients.size(); ++i) {
+                        if (clients[i] != sd && FD_ISSET(clients[i], &write_fds)) {
+                            if (send(clients[i], buffer, strlen(buffer), 0) == -1) {
+                                perror("send");
+                            }
+                        }
+                    }
+                } else if (valread == 0) {
+                    std::cout << "Client disconnected" << std::endl;
+                    close(sd);
+                    it = clients.erase(it);
+                    continue;
+                } else {
+                    perror("read error");
+                    close(sd);
+                    it = clients.erase(it);
+                    continue;
+                }
             }
+            ++it;
         }
     }
 
